2bb6b7e18557fb94357b96230b1b9cb939cdbb3d
Dhruv Rai <dhruv156rai@gmail.com>
2026-06-19 16:45:10 +0530

    generated reusable base model and created user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Boolean, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models.base_model import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    username: Mapped[str] = mapped_column(
+        String(30),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+
+    hashed_password: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+        nullable=False,
+    )
+
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False,
+    )
